092e469d9e799cf85f2bd9233ba6f4e2fcb7051c
Saksham tripathi <tripathisaksham000@gmail.com>
2025-07-12 14:09:59 +0530

    Upload C++ Project

---
+++ Guess the number game .cpp/c++.cpp
@@ -0,0 +1,33 @@
+ #include <iostream>
+#include <cstdlib>    
+#include <ctime>      
+
+using namespace std;
+
+int main() {
+    int number, guess, attempts = 0;
+    
+    
+    srand(time(0));
+    number = rand() % 100 + 1;   ```
+
+    cout << " Welcome to the Number Guessing Game!" << endl;
+    cout << "Guess a number between 1 and 100" << endl;
+
+  
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+        attempts++;
+
+        if (guess > number) {
+            cout << "Too high! Try again. " << endl;
+        } else if (guess < number) {
+            cout << "Too low! Try again. " << endl;
+        } else {
+            cout << "Congratulations! You guessed the number in " << attempts << " attempts." << endl;
+         }
+    } while (guess != number);
+
+    return 0;
+}
